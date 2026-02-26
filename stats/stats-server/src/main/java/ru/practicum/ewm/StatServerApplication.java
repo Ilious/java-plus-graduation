5edd0d320492b15a28127bb4ed3371adbfd16507
ilnurtuhtarov <something82972@gmail.com>
2026-02-26 19:54:56 +0400

    config + eureka

--- stats/stats-server/src/main/java/ru/practicum/ewm/StatServerApplication.java
+++ stats/stats-server/src/main/java/ru/practicum/ewm/StatServerApplication.java
@@ -2,10 +2,12 @@ package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
-public class StatService {
+public class StatServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(StatService.class, args);
+        SpringApplication.run(StatServerApplication.class, args);
     }
 }
