18aa25d67e91a33bd229f29bdebe43d302a58223
I1nur <tuxtarov0475@gmail.com>
2026-03-03 17:59:58 +0400

    fix interaction-api/pom.xml and add annotations to internalOperations interfaces

--- core/interaction-api/src/main/java/ru/practicum/feign/operation/CommentInternalOperations.java
+++ core/interaction-api/src/main/java/ru/practicum/feign/operation/CommentInternalOperations.java
@@ -1,10 +1,13 @@
 package ru.practicum.feign.operation;
 
+import jakarta.validation.constraints.Positive;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Validated
 public interface CommentInternalOperations {
 
     @GetMapping("/{eventId}")
-    Long getCountPublishedCommentsByEventId(@PathVariable Long eventId);
+    Long getCountPublishedCommentsByEventId(@PathVariable @Positive Long eventId);
 }
--- core/interaction-api/src/main/java/ru/practicum/feign/operation/UserInternalOperations.java
+++ core/interaction-api/src/main/java/ru/practicum/feign/operation/UserInternalOperations.java
@@ -1,5 +1,8 @@
 package ru.practicum.feign.operation;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,11 +10,12 @@ import ru.practicum.dto.UserShortDto;
 
 import java.util.List;
 
+@Validated
 public interface UserInternalOperations {
 
     @GetMapping("/{userId}")
-    UserShortDto getUserById(@PathVariable Long userId);
+    UserShortDto getUserById(@PathVariable @Positive Long userId);
 
     @GetMapping
-    List<UserShortDto> getUsersByIds(@RequestParam List<Long> ids);
+    List<UserShortDto> getUsersByIds(@RequestParam @NotNull List<Long> ids);
 }
